85212284f1ec9dbb2e7a92dfb6b83b9de0d9f5cb
ILMA GULZAR <ilmagulzar2@gmail.com>
2026-01-25 20:18:16 +0530

    Add files via upload

---
+++ DSA/Binary_Decimal.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class   Binary_Decimal{
+    static void DecimalToBinary(int binary) {
+         int pow=1;
+         int decimal = 0;
+        while(binary >0) {
+            int digit=binary %10;
+           decimal=decimal+digit*pow;
+           pow=pow*2;
+           binary = binary/10;
+        }
+          System.out.println("Binary = "+decimal);
+      }
+    public static void main(String args[]) {
+    Scanner sc= new Scanner(System.in);
+    System.out.print("Enter the value of num = ");
+    int num=sc.nextInt();
+    DecimalToBinary(num); 
+    sc.close();
+}
+}
