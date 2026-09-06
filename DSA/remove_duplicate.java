ae314fa840de63138362c5658c8e851b2fbda743
ILMA GULZAR <ilmagulzar2@gmail.com>
2026-09-06 14:32:53 +0530

    Implement duplicate character removal in Java
    Added a Java program to remove duplicate characters from a string.

--- DSA/remove_duplicate.java
+++ DSA/remove_duplicate.java
@@ -1,1 +1,26 @@
 
+import java.util.Scanner;
+class Str {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a String = ");
+        String str = sc.next();
+        String newStr = "";
+
+        for(int i=0;i<str.length();i++) {
+            boolean found = false;
+            for(int j=0;j<newStr.length();j++) {
+                if(str.charAt(i) == newStr.charAt(j)) {
+                    found = true;
+                    break;
+                }
+        
+            }
+            if(found == false) {
+                newStr=newStr+str.charAt(i);
+            }
+        }
+          System.out.println("New String = " + newStr);
+    }
+}
